2a69872097f7c004659fcb41eb097a3f6ca456c1
SmartBooks <mr.wangya@qq.com>
2017-11-19 21:14:20 +0800

    ...

--- lexicon-resolve/src/main/java/com/github/smartbooks/ParseSogo.java
+++ lexicon-resolve/src/main/java/com/github/smartbooks/ParseSogo.java
@@ -1,6 +1,8 @@
 package com.github.smartbooks;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Paths;
@@ -53,6 +55,14 @@ public class ParseSogo {
                 if (line.trim().length() > 0) {
                     line = java.net.URLDecoder.decode(line, "utf-8");
                     sw.write(String.format("%s\n", line));
+
+                    String filename = line.split("=")[2] + ".scel";
+
+                    try {
+                        downLoadFromUrl(line, filename, "E:\\github\\lexicon\\data\\scel");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
             br.close();
@@ -106,6 +116,65 @@ public class ParseSogo {
         }
         raf.close();
         System.out.println("生成txt成功！,总计写入: " + count + " 条数据！");
+    }
+
+    /**
+     * 从网络Url中下载文件
+     *
+     * @param urlStr
+     * @param fileName
+     * @param savePath
+     * @throws IOException
+     */
+    public static void downLoadFromUrl(String urlStr, String fileName, String savePath) throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        //设置超时间为3秒
+        conn.setConnectTimeout(3 * 1000);
+        //防止屏蔽程序抓取而返回403错误
+        conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+
+        //得到输入流
+        InputStream inputStream = conn.getInputStream();
+        //获取自己数组
+        byte[] getData = readInputStream(inputStream);
+
+        //文件保存位置
+        File saveDir = new File(savePath);
+        if (!saveDir.exists()) {
+            saveDir.mkdir();
+        }
+        File file = new File(saveDir + File.separator + fileName);
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(getData);
+        if (fos != null) {
+            fos.close();
+        }
+        if (inputStream != null) {
+            inputStream.close();
+        }
+
+
+        System.out.println("info:" + url + " download success");
+
+    }
+
+    /**
+     * 从输入流中获取字节数组
+     *
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    public static byte[] readInputStream(InputStream inputStream) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        while ((len = inputStream.read(buffer)) != -1) {
+            bos.write(buffer, 0, len);
+        }
+        bos.close();
+        return bos.toByteArray();
     }
 
 }
